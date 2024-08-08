e98b6f341d3c816e229e52edfdcba8634f600168
cchompoottr <chompoo99075@gmail.com>
2024-08-09 03:01:51 +0700

    base

---
+++ src/App.js
@@ -0,0 +1,34 @@
+function App() {
+  return (
+    <body className="w-full">
+
+    <div className="text-center text-black font-normal border-solid border-b-[2px] border-black p-5">
+      <h1 className="text-2xl">cchompoo</h1>
+    </div>
+
+    <div className="flex justify-center -pt-20 -mt-10">
+      <img src="../hunter.png" alt="pic" className="py-20 w-auto h-[500px] rounded-lg"/>
+    </div>
+
+    <h1 className="text-center font-light text-2xl -pt-10 -mt-10"><span className="font-medium">Hello,</span><br></br>
+    I am Software Engineering Student</h1>
+
+    <h1 className="title text-center text-3xl py-10 mt-10">ABOUT ME</h1>
+    <p className="text-center text-base font-light">My name is Thitirat Rattanapan, you can call me Chompoo.<br></br>
+    I am a 4th-year Software Engineering student at Thammasat University.<br></br> 
+    <span className="pt-5 mt-5 block">Passionate about technology and eager to apply my skills in real-world projects,<br></br>
+    I am dedicated to continuous learning and making a meaningful impact in the tech industry.</span></p>
+
+    <h1 className="title text-center text-3xl pt-10 mt-10 pb-5">CONTACT ME</h1>
+    <p className="text-center text-base font-light leading-loose pb-10 mb-10">chompoo99075@gmail.com<br></br>
+        098-072-9984<br></br>
+        https://www.linkedin.com/in/thitirat-rattanapan-b757422a1/</p>
+
+
+
+    </body>
+  );
+}
+
+export default App;
+
